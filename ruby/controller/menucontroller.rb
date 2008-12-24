eeac88a6f079db6ca4eaf88339efe8c2beced94e
Satoshi Nakagawa <psychs@limechat.net>
2008-12-25 03:28:04 +0900

    clean up

--- ruby/controller/menucontroller.rb
+++ ruby/controller/menucontroller.rb
@@ -300,11 +300,8 @@ class MenuController
     end
   end
 
-  def preferenceDialog_onOk(sender, m)
-    preferences.save
-  end
-
   def preferenceDialog_onClose(sender)
+    preferences.save
     @pref_dialog = nil
   end
 
